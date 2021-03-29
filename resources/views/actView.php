d4768ec136be8a97257eb69fb02a52509fc76410
Kim John A. Hidalgo <kimpoyhidalgo@gmail.com>
2021-03-29 17:34:06 +0800

    bigger modal

--- resources/views/actView.php
+++ resources/views/actView.php
@@ -112,7 +112,7 @@ $progress = $app->areaProgress($_GET['cycle'], $_GET['area']);
     </div>
 </div>
 <div class="modal fade" id="modalViewActivity" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title activity_title"></h5>
--- resources/views/actView_rpmo.php
+++ resources/views/actView_rpmo.php
@@ -116,7 +116,7 @@ if(!$area==false){
         </div>
     </div>
     <div class="modal fade" id="modalViewActivity" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title activity_title"></h5>
