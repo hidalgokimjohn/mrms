16aed8431bd96e85e4d1fce85f95f5b45a765b21
hidalgokimjohn <kimpoyhidalgo@gmail.com>
2021-06-24 09:56:03 +0800

    added loading screen in dqa_compliance

--- resources/views/tblCompliance.php
+++ resources/views/tblCompliance.php
@@ -35,6 +35,9 @@
                         <div class="col-sm-4">
                             <div class="card">
                                 <div class="card-body">
+                                    <div class="spinner-border text-primary mr-2" role="status">
+                                                <span class="sr-only">Loading...</span>
+                                    </div>
                                     <div id="displayFindings">
 
                                     </div>
