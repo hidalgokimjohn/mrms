60a49572f21d0ad3921cc8292a576a671ef76f8d
hidalgokimjohn <kimpoyhidalgo@gmail.com>
2021-07-06 11:44:36 +0800

    Update tbl_dqaCompliance.php

--- resources/ajax/tbl_dqaCompliance.php
+++ resources/ajax/tbl_dqaCompliance.php
@@ -1,7 +1,9 @@
 <?php
 include_once("../../app/Database.php");
 include_once("../../app/App.php");
+include_once("../../app/DataQualityAssessment.php");
 include_once("../../app/Auth.php");
 $auth = new \app\Auth();
 $app = new \app\App();
-$app->tbl_dqaCompliance();
+$dqa = new \app\DataQualityAssessment();
+$dqa->tbl_dqa_compliance();
