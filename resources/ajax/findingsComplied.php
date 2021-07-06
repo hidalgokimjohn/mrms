3dfda14822dda54dd09b2b7a54977a2f3a4a1d4e
hidalgokimjohn <kimpoyhidalgo@gmail.com>
2021-07-06 11:42:29 +0800

    Create findingsComplied.php

---
+++ resources/ajax/findingsComplied.php
@@ -0,0 +1,9 @@
+<?php
+include_once("../../app/Database.php");
+include_once("../../app/App.php");
+include_once("../../app/DataQualityAssessment.php");
+include_once("../../app/Auth.php");
+$auth = new \app\Auth();
+$app = new \app\App();
+$dqa = new \app\DataQualityAssessment();
+$dqa->findings_complied();
